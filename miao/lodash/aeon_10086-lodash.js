39d77deef538b2e2da96110c47b22c30ca4bb50b
Aeon <aeon_1024@163.com>
2020-12-08 16:55:54 +0800

    update lodash

--- miao/lodash/aeon_10086-lodash.js
+++ miao/lodash/aeon_10086-lodash.js
@@ -62,13 +62,121 @@ var aeon_10086 = (function () {
 
     return res;
   }
+  //比较 ary 和 args 中的函数
+  function differenceWith(ary, args = [], callback) {
+    let res = [];
+    /* for (let item of ary) {
+      if (typeof item !== "object" && args.indexOf(item) == -1) {
+        res.push(item);
+      } else {
+        let flag = true;
+        for (let j = 0; j < args.length; j++) {
+          if (DeepComparsion(item, args[j])) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) res.push(item);
+      }
+    } */
+    for (let item of ary) {
+      let flag = true;
+      for (let test of args) {
+        if (callback(item, test)) flag = false;
+      }
+      if (flag) res.push(item);
+    }
+    return res;
+  }
+  // join 数组中的 join
+  function join(ary, str) {
+    let res = "";
+    for (let i = 0; i < ary.length - 1; i++) {
+      res += ary[i] + str;
+    }
+    res += ary[ary.length - 1];
+    return res;
+  }
+  // last 返回数组中的最后一个元素
+  function last(ary) {
+    if (ary.length < 1) return [];
+    return ary[ary.length - 1];
+  }
+  // lastIndexOf
+  function lastIndexOf(ary, val, fromIndex = ary.length - 1) {
+    for (let i = fromIndex; i >= 0; i--) {
+      if (ary[i] === val) return i;
+    }
+    return -1;
+  }
+  // drop
+  function drop(ary, n = 1) {
+    let res = [];
+    for (let i = n; i < ary.length; i++) {
+      res.push(ary[i]);
+    }
+    ary = [...res];
+    return ary;
+  }
+  //删除后n个元素
+  function dropRight(ary, n = 1) {
+    let res = [];
+    for (let i = 0; i < ary.length - n; i++) {
+      res.push(ary[i]);
+    }
+    ary = [...res];
+    return ary;
+  }
+  //填充
+  function fill(ary, val,start = 0, end = ary.length){
+    for(let i = start, i < end; i++){
+      ary[i] = val;
+    }
+    return ary;
+  }
+  //工具函数
+  function DeepComparsion(obj1, obj2) {
+    for (key in obj1) {
+      if (typeof obj1[key] != "object" && typeof obj1[key] != "object") {
+        if (obj1[key] != obj2[key]) return false;
+      } else {
+        if (!DeepComparsion(obj1[key], obj2[key])) return false;
+      }
+    }
+    return true;
+  }
+  function arraryComparsion(ary1, ary2) {
+    if (ary1.length !== ary2.length) return false;
+    for (let i = 0; i < ary1.length; i++) {
+      if (typeof ary1[i] !== typeof ary2[i]) return false;
+      if (
+        Array.isArray(ary1[i]) &&
+        Array.isArray(ary2[i]) &&
+        !arraryComparsion(ary1[i], ary2[i])
+      ) {
+        return false;
+      }
+      if (typeof ary1[i] === "object" && !DeepComparsion(ary1[i], ary2[i])) {
+        return false;
+      } else if (ary1[i] !== ary2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
   return {
     chunk,
     compact,
     concat,
     difference,
     differenceBy,
+    differenceWith,
+    join,
+    last,
+    lastIndexOf,
+    drop,
+    dropRight,
   };
 })();
-const TESTRES = aeon_10086.difference([1, 2, 3, 4], [1, 3], [4]);
+const TESTRES = aeon_10086.dropRight([1, 32, 5, 23, 3], 0);
 console.log(TESTRES);
