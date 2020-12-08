202a6f2a8837bc2afe7394c67ed1964e68c683f9
Aeon <aeon_1024@163.com>
2020-12-08 16:02:18 +0800

    update lodash

--- miao/lodash/aeon_10086-lodash.js
+++ miao/lodash/aeon_10086-lodash.js
@@ -1,4 +1,5 @@
 var aeon_10086 = (function () {
+  //将数组分块
   function chunk(ary, size = 1) {
     if (ary.length < 1) return [];
     size = size > 0 ? size : 1;
@@ -13,6 +14,7 @@ var aeon_10086 = (function () {
     }
     return res;
   }
+  //将数组中非真的值过滤
   function compact(ary) {
     let res = [];
     for (let i = 0; i < ary.length; i++) {
@@ -20,6 +22,7 @@ var aeon_10086 = (function () {
     }
     return res;
   }
+  //拼接数组
   function concat(ary, ...args) {
     let res = [...ary];
     for (let i = 0; i < args.length; i++) {
@@ -31,13 +34,32 @@ var aeon_10086 = (function () {
     }
     return res;
   }
-  function difference(ary, args) {
+  //过滤数组中与其他数组中值相同的部分
+  function difference(ary, ...args) {
+    let grounp = concat([], ...args);
     let res = [];
     for (let i = 0; i < ary.length; i++) {
-      if (args.indexOf(ary[i]) == -1) {
+      if (grounp.indexOf(ary[i]) == -1) {
         res.push(ary[i]);
       }
     }
+    return res;
+  }
+  // 待完善
+  function differenceBy(ary, args = [], callback) {
+    let res = [];
+    // 当传入的值为函数时
+    if (typeof callback == "function") {
+      for (let i = 0; i < args.length; i++) {
+        args[i] = callback(args[i]);
+      }
+      for (let i = 0; i < ary.length; i++) {
+        if (args.indexOf(callback(ary[i])) == -1) {
+          res.push(ary[i]);
+        }
+      }
+    }
+
     return res;
   }
   return {
@@ -45,7 +67,8 @@ var aeon_10086 = (function () {
     compact,
     concat,
     difference,
+    differenceBy,
   };
 })();
-const TESTRES = aeon_10086.difference([1, 3, 5], [2, 3, 1]);
+const TESTRES = aeon_10086.difference([1, 2, 3, 4], [1, 3], [4]);
 console.log(TESTRES);
